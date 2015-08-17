ed8324fd789c803e4fcd879f8f5d1dd344635688
Alex <alex1820m@gmail.com>
2015-08-16 22:02:31 -0500

    home

--- app/views/desktop/home.blade.php
+++ app/views/desktop/home.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.desktop')
 @section('content')
 <a href="{{url()}}/sjp" title="Sarah Jessica Parker">
-	<img src="{{url()}}/img/home/1.jpg" class="img-responsive center-block" alt="Sarah Jessica Parker" />
+	<img src="{{url()}}/img/home/sarah.jpg" class="img-responsive center-block" alt="Sarah Jessica Parker" />
 </a>
 <a href="{{url()}}/marcas" title="Marcas">
-	<img src="{{url()}}/img/home/2.jpg" class="img-responsive center-block" alt="Marcas" />
+	<img src="{{url()}}/img/home/marcas.jpg" class="img-responsive center-block" alt="Marcas" />
 </a>
 <a href="{{url()}}/lookbook" title="Lookbook">
-	<img src="{{url()}}/img/home/3.jpg" class="img-responsive center-block" alt="Lookbook" />
+	<img src="{{url()}}/img/home/lookbook.jpg" class="img-responsive center-block" alt="Lookbook" />
 </a>
 <a href="{{url()}}/accesorios" title="Accesorios">
-	<img src="{{url()}}/img/home/4.jpg" class="img-responsive center-block" alt="Accesorios" />
+	<img src="{{url()}}/img/home/accesorios.jpg" class="img-responsive center-block" alt="Accesorios" />
 </a>
 @stop
 
@@ -21,13 +21,13 @@
 <!-- Google -->
 <meta itemprop="name" content="Yo elijo las mejores marcas - Tendencias 2015 - Ripley">
 <meta itemprop="description" content="Descubre las últimas tendencias de la moda, temporada Primavera Verano 2015 - 2016, solo en Ripley. ">
-<meta itemprop="image" content="{{url()}}/img/home/1.jpg">
+<meta itemprop="image" content="{{url()}}/img/home/sarah.jpg">
 
 <!-- Open Graph data -->
 <meta property="og:title" content="Yo elijo las mejores marcas - Tendencias 2015 - Ripley" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="{{url()}}/" />
-<meta property="og:image" content="{{url()}}/img/home/1.jpg" />
+<meta property="og:image" content="{{url()}}/img/home/sarah.jpg" />
 <meta property="og:description" content="Descubre las últimas tendencias de la moda, temporada Primavera Verano 2015 - 2016, solo en Ripley. " />
 <meta property="og:site_name" content="Descubre las últimas tendencias de la moda, temporada Primavera Verano 2015 - 2016, solo en Ripley. " />
 <meta property="fb:admins" content="100008010128050" />
