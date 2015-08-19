69dd9a8731795e639d52c7af66216077c02329a9
Alex <alex1820m@gmail.com>
2015-08-19 11:42:54 -0500

    accesorios

--- app/views/desktop/accesorios-detalle.blade.php
+++ app/views/desktop/accesorios-detalle.blade.php
@@ -24,14 +24,14 @@
 <?php
 switch ($id) {
 	case 1:
-		$marca = "<p>'barbados.png'</p>";
+		$marca = 'barbados.png';
 		$nombre = "<p>Vestido a rayas<br/> frunce</p>";
 		$sku = "cod.93728639";
 		$precio = '<span>PRECIO NORMAL </span><span class="red">$ 79.9</span>';
 		$imagen = 'Sandalia-Tabihebi-marquis.jpg';
 		break;
 	case 2:
-		$marca = "<p>'barbados.png'</p>";
+		$marca = 'barbados.png';
 		$nombre = "<p>Zapato Iris</p>";
 		$sku = "cod.16909255";
 		$precio = '<span>PRECIO NORMAL </span><span class="red">$99.9</span>';
@@ -97,10 +97,12 @@ switch ($id) {
 
 <section id="accesorios-1">
 	<div class="accesorios-detalle-lightbox">
-		<img src="{{url()}}/img/<?php echo($imagen); ?>" class="img-responsive" alt="Accesorios" />
+		<img src="{{url()}}/img/accesorios/<?php echo($imagen); ?>" class="img-responsive" alt="Accesorios" />
 		<div class="precio-accesorios clearfix">
 			<div class="columna-precio">
-				<img src="{{url()}}/img/accesorios/<?php echo($marca); ?>" class="img-responsive" alt="Accesorios" />
+				<p>
+					<img src="{{url()}}/img/accesorios/<?php echo($marca); ?>" class="img-responsive" alt="Accesorios" />
+				</p>
 			</div>
 			<div class="columna-precio accesorios-centro">
 				<p><?php echo($nombre); ?><?php echo($sku); ?></p>
