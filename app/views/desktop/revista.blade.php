0ed675aeb812244c4c4061df71a3323d9ed8005a
appsmambo <quintanilla.peru@gmail.com>
2015-08-20 18:32:28 -0500

    ajustes
    update del codigo isu en revista

--- app/views/desktop/revista.blade.php
+++ app/views/desktop/revista.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.desktop')
 @section('content')
 <div class="embed-responsive embed-responsive-16by9">
-  <iframe class="embed-responsive-item" src="//e.issuu.com/embed.html#0/14766120" ></iframe>
+  <iframe class="embed-responsive-item" src="//e.issuu.com/embed.html#0/14907484" ></iframe>
 </div>
 @stop
